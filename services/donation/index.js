d96670e2afb3b9cc22cf2ea6b83333f02969ef6a
Jan Stránský <jan.stransky@arnal.cz>
2018-08-15 23:40:43 +0200

    Cleanup

--- services/donation/index.js
+++ services/donation/index.js
@@ -9,10 +9,6 @@ class Donation extends Service {
   async donate (addr, amount, msg, from) {
     return this._send(addr, 'donation', 'make_donation', [msg], { from, value: amount })
   }
-
-	async balanceOf (addr, owner) {
-    return this._call(addr, 'donation', 'balanceOf', [owner])
-  }
 }
 
 module.exports = {
